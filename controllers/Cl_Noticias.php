99ba8a3d3f1aa65e3b412921bace7829a390b0c1
Kyria77 <vermonca@hotmail.com>
2025-04-29 16:25:58 +0200

    Clase noticias con búsqueda de las noticias de la base de datos

---
+++ controllers/Cl_Noticias.php
@@ -0,0 +1,66 @@
+<?php
+require_once __DIR__ . '/../config/config.php';
+require_once 'db_conn.php';
+
+class Noticia{
+    public function __construct(){
+        if(session_status() == PHP_SESSION_NONE){
+            session_start();
+        }
+    }
+
+    //Función para buscar y leer todas las noticias de la base de datos con todos sus datos.
+    public function leerNoticia($mysqli_connection){
+        $select_stmt = null;
+    
+        try{
+            $select_stmt = $mysqli_connection->prepare('SELECT * FROM noticias');
+    
+            if($select_stmt === false){
+                error_log("No se pudo preparar la sentencia" . $mysqli_connection->error);
+                //$exception_error = true;
+                echo "No se pudo preparar la sentencia";
+                return false;
+            }
+    
+            if(!$select_stmt->execute()){
+                error_log(("No se pudo ejecutar la sentencia " . $mysqli_connection->error));
+                //$exception_error = true;
+                echo "No se pudo ejecutar la sentencia";
+                return false;
+            }
+    
+            $result = $select_stmt->get_result();
+            $noticias = [];
+    
+            if($result->num_rows > 0){
+                while($fila = $result->fetch_assoc()){
+                    $noticias[] = $fila;
+                    echo "<br>";
+                }
+                echo "Estoy leyendo las noticias";
+                return $noticias;
+            }else{
+                echo "No hay noticias disponibles";
+                return false;
+            }
+        }catch(Exception $e){
+            error_log("Error al ejecutar la función: " . $e->getMessage());
+            //$exception_error = true;
+            return false;
+        } finally{
+            if($select_stmt !== null){
+                $select_stmt->close();
+            }
+        }
+    }
+    
+}
+
+$noticias = new Noticia();
+$noticiaL = $noticias->leerNoticia($mysqli_connection);
+print_r($noticiaL);
+
+
+
+?>
